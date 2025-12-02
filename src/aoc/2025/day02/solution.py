8d81bd9c92a115989074b7db328422f5653bc688
Jared Bothwell <jared.bothwell@hudl.com>
2025-12-01 22:29:02 -0600

    update template

--- src/aoc/2025/day02/solution.py
+++ src/aoc/2025/day02/solution.py
@@ -5,8 +5,5 @@ def part1(input_data):
 def part2(input_data):
     pass
 
-if __name__ == "__main__":
-    with open("input.txt") as infile:
-        input_data = infile.read()
-        print("Part 1:", part1(input_data))
-        print("Part 2:", part2(input_data))
+def solve(input_data):
+    return part1(input_data), part2(input_data)
