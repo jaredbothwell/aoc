2e59280c866a597f71c377e63ee91848cb2170b4
Jared Bothwell <jared.bothwell@hudl.com>
2025-12-01 22:28:30 -0600

    scaffold day 2

---
+++ src/aoc/2025/day02/solution.py
@@ -0,0 +1,12 @@
+
+def part1(input_data):
+    pass
+
+def part2(input_data):
+    pass
+
+if __name__ == "__main__":
+    with open("input.txt") as infile:
+        input_data = infile.read()
+        print("Part 1:", part1(input_data))
+        print("Part 2:", part2(input_data))
