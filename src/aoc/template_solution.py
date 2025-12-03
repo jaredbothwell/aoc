70ecb217956578095bc8e909d6192a02de9853e4
Jared Bothwell <jared.bothwell@hudl.com>
2025-12-03 16:36:51 -0600

    update solution template

--- src/aoc/template_solution.py
+++ src/aoc/template_solution.py
@@ -1,10 +1,15 @@
-def part1(input_data: str) -> int:
+def part1(parsed_data: str) -> int:
     pass
 
 
-def part2(input_data: str) -> int:
+def part2(parsed_data: str) -> int:
+    pass
+
+
+def parse_input(input_data: str):
     pass
 
 
 def solve(input_data: str) -> tuple[int, int]:
-    return part1(input_data), part2(input_data)
+    parsed_data = parse_input(input_data)
+    return part1(parsed_data), part2(parsed_data)
